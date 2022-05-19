618ed8e5a5cac18f213154604cd7ff93a5063de0
Shahzad <mech1090@gmail.com>
2022-05-18 19:32:17 -0600

    signup not working

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,9 +1,21 @@
+const bcrypt = require('bcrypt')
+const config = require('config')
+const userModel = require('../models/Users')
+
 const getLoginForm = (req,res)=>{
     res.render('login/layout')
 }
 const login = (req,res)=>{}
-const getSignupForm = (req,res)=>{}
-const signUp = (req,res)=>{}
+const getSignupForm = (req,res)=>{
+    res.render('signup/layout')
+}
+const signUp = async(req,res)=>{
+    const {email,password} = req.body
+    const passwordHash = await bcrypt.hash(password,config.get('hashing.salt'))
+    const user = await userModel.create({email,password:passwordHash})
+    console.log(email,passwordHash)
+    res.send(user)
+}
 
 
 module.exports = {getLoginForm,login,getSignupForm,signUp}
