77f2511e7a6579879e0cce7ae0dfbc397bd10a02
Shahzad <mech1090@gmail.com>
2022-05-19 12:05:38 -0600

    completed login and signup page

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,20 +1,39 @@
 const bcrypt = require('bcrypt')
 const config = require('config')
+const User = require('../models/Users')
 const userModel = require('../models/Users')
 
 const getLoginForm = (req,res)=>{
     res.render('login/layout')
 }
-const login = (req,res)=>{}
+const login = async(req,res)=>{
+    try{
+        const{email,password} = req.body
+        //   const passwordHash = await bcrypt.hash(password,config.get('hashing.salt'))
+           user = await User.findOne({email})
+           const isuthorized = await bcrypt.compare(password,user.password)
+           if(isuthorized){
+               return res.render('login/layout',{message:'Login Successful'})
+            }
+            return res.render('login/layout',{message:'Email not Found'})
+    }catch(error){
+        res.render('login/layout',{message:'Error while Login'})
+    }
+
+}
+
 const getSignupForm = (req,res)=>{
     res.render('signup/layout')
 }
 const signUp = async(req,res)=>{
-    const {email,password} = req.body
-    const passwordHash = await bcrypt.hash(password,config.get('hashing.salt'))
-    const user = await userModel.create({email,password:passwordHash})
-    console.log(email,passwordHash)
-    res.send(user)
+    try{
+        const {email,password} = req.body
+        const passwordHash = await bcrypt.hash(password,config.get('hashing.salt'))
+        const user = await userModel.create({email,password:passwordHash})
+        return res.render('signup/layout',{message:'Signup successful'})
+    }catch(error){
+        res.render('signup/layout',{message:'Error while Signup'})
+    }
 }
 
 
